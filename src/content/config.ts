43a7ff3c553ad4940bf111e88188f838dd5a332b
Jacob Packert <jacob.packert@lego.com>
2024-03-29 14:24:58 +0100

    feat/blog section (#2)
    * feat: add blog dynamic page route
    
    * feat: add content collection and first blog post md file
    
    * feat: migrate remaining blog posts and img assets
    
    * feat: support mdx, fix images
    
    * fix: revert gif article to use standard markdown
    
    * fix: wrong import path for gifs
    
    * fix: gifs in public folder
    
    * fix: wrong import
    
    * feat: add sharp as explicit dependency
    
    * feat: try again with Picture
    
    * fix: remember to commit the gif in /assets
    
    * chore: refactor more images
    
    * chore: remove unnecessary duplicated images

---
+++ src/content/config.ts
@@ -0,0 +1,20 @@
+import { z, defineCollection } from 'astro:content';
+
+const blogSchema =
+  z.object({
+    title: z.string(),
+    date: z.string(),
+    description: z.string(),
+  });
+
+// Define collection and schema validation for blog
+const blogCollection = defineCollection({
+  type: 'content',
+  schema: blogSchema
+ });
+
+// 3. Export a single `collections` object to register your collection(s)
+//    This key should match your collection directory name in "src/content"
+export const collections = {
+  'blog': blogCollection,
+};
