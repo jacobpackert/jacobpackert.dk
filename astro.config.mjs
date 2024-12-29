9e5dfa8c9dc0b341b7b6003d3484d8434077d63e
Jacob Packert <jacobpackert@gmail.com>
2024-12-29 21:32:14 +0100

    feat/add simple api route (#17)
    * feat: add astro netlify adapter
    
    * feat: add simple API route
    
    * feat: opt out of netlify image cdn

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,4 +11,7 @@ export default defineConfig({
     },
   },
   prefetch: true,
+  adapter: netlify({
+    imageCDN: false,
+  }),
 });
---
+++ src/pages/api/[id].ts
@@ -0,0 +1,12 @@
+import type { APIRoute } from 'astro';
+
+export const prerender = false;
+
+export const GET: APIRoute = async ({ params, request } ) => {
+  
+  return new Response(
+    JSON.stringify({
+      count: params.id,
+    })
+  )
+}
